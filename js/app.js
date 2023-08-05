137ecbe5fdff49ddf5164352c34f36be4727c2fb
Jean Pool Gutierrez <jean-gutierrez-117@hotmail.com>
2023-08-05 16:58:27 -0500

    26. Validación en Bootstrap

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const modalImagen = document.querySelector('#modal-imagen');
-modalImagen.addEventListener('show.bs.modal', function(event){
+
+if(modalImagen) {
+    modalImagen.addEventListener('show.bs.modal', function(event){
     const enlace = event.relatedTarget;
     const rutaImagen = enlace.getAttribute('data-bs-imagen');
     // Construir la imagen
@@ -18,3 +20,26 @@ modalImagen.addEventListener('hidden.bs.modal', function(){
     const contenidoModal = document.querySelector('.modal-body');
     contenidoModal.textContent = "";
 });
+}
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
