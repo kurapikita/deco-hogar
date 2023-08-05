19a39329f5b348a996dd8d28d830a5c5a20bbdca
Jean Pool Gutierrez <jean-gutierrez-117@hotmail.com>
2023-08-04 22:25:33 -0500

    24. Finalizando nuestra ventana modal dinamica

--- js/app.js
+++ js/app.js
@@ -2,5 +2,19 @@ const modalImagen = document.querySelector('#modal-imagen');
 modalImagen.addEventListener('show.bs.modal', function(event){
     const enlace = event.relatedTarget;
     const rutaImagen = enlace.getAttribute('data-bs-imagen');
-    console.log(rutaImagen);
-})
+    // Construir la imagen
+    const imagen = document.createElement('IMG');
+    imagen.src = `images/${rutaImagen}.jpg`;
+    imagen.classList.add('img-fluid');
+    imagen.alt = "Imagen de la galería";
+
+    const contenidoModal = document.querySelector('.modal-body');
+    contenidoModal.appendChild(imagen);
+
+});
+
+
+modalImagen.addEventListener('hidden.bs.modal', function(){
+    const contenidoModal = document.querySelector('.modal-body');
+    contenidoModal.textContent = "";
+});
